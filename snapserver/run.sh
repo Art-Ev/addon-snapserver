b45d117c349a07b974aca77fccaeea409764ff3e
Arthur Evrard <83211842+Art-Ev@users.noreply.github.com>
2022-11-27 19:35:23 +0100

    Add files via upload

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -67,7 +67,8 @@ sed -i "/#threads = -1/a threads = ${threads}" "${config}"
 datadir=$(bashio::config 'server_datadir')
 sed -i "/#datadir =/a datadir = ${datadir}" "${config}"
 
-cat "${config}"
+mapfile < "${config}"
+bashio::log.info "${mapfile[@]}"
 
 bashio::log.info "Starting SnapServer..."
 
