fced399cc7cb9c9dbcc94b68722ddb32673e142f
Arthur Evrard <83211842+Art-Ev@users.noreply.github.com>
2022-11-27 18:42:47 +0100

    Add files via upload

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -26,47 +26,46 @@ bashio::log.info "Populating snapserver.conf..."
 
 # Streams
 streams=$(bashio::config 'streams')
-sed "/[stream]/a $(streams)" "${config}"
+sed "/[stream]/a ${streams}" "${config}"
 
 # Stream bis and ter
 if bashio::config.has_value 'stream_bis'; then
     stream_bis=$(bashio::config 'stream_bis')
-    sed "/[stream]/a $(stream_bis)" "${config}"
+    sed "/[stream]/a ${stream_bis}" "${config}"
 fi
 if bashio::config.has_value 'stream_ter'; then
     stream_ter=$(bashio::config 'stream_ter')
-    sed "/[stream]/a $(stream_ter)" "${config}"
+    sed "/[stream]/a ${stream_ter}" "${config}"
 fi
 
 # Buffer
 buffer=$(bashio::config 'buffer')
-bashio::log.info "$(bufffer)"
-sed -i "/#buffer = 1000/a buffer = $(bufffer)" "${config}"
+sed -i "/#buffer = 1000/a buffer = ${bufffer}" "${config}"
 # Codec
 codec=$(bashio::config 'codec')
-sed -i "/#codec = flac/a codec = $(codec)" "${config}"
+sed -i "/#codec = flac/a codec = ${codec}" "${config}"
 # Muted
 muted=$(bashio::config 'send_to_muted')
-sed -i "/#send_to_muted = false/a codec = $(muted)" "${config}"
+sed -i "/#send_to_muted = false/a codec = ${muted}" "${config}"
 # Sampleformat
 sampleformat=$(bashio::config 'sampleformat')
-sed -i "/#sampleformat = 48000:16:2/a codec = $(sampleformat)" "${config}"
+sed -i "/#sampleformat = 48000:16:2/a codec = ${sampleformat}" "${config}"
 # Http
 http=$(bashio::config 'http_enabled')
-sed "/[http]/a enabled = $(http)" "${config}"
+sed "/[http]/a enabled = ${http}" "${config}"
 sed -i "/#bind_to_address = 0.0.0.0/a bind_to_address = ::" "${config}"
 # TCP
 tcp=$(bashio::config 'tcp_enabled')
-sed "/[TCP]/a enabled = $(TCP)" "${config}"
+sed "/[TCP]/a enabled = ${TCP}" "${config}"
 # Logging
 logging=$(bashio::config 'logging_enabled')
-sed "/[logging]/a debug = $(logging)" "${config}"
+sed "/[logging]/a debug = ${logging}" "${config}"
 # Threads
 threads=$(bashio::config 'server_threads')
-sed -i "/#threads = -1/a threads = $(threads)" "${config}"
+sed -i "/#threads = -1/a threads = ${threads}" "${config}"
 # Datadir
 datadir=$(bashio::config 'server_datadir')
-sed -i "/#datadir =/a datadir = $(datadir)" "${config}"
+sed -i "/#datadir =/a datadir = ${datadir}" "${config}"
 
 bashio::log.info "Starting SnapServer..."
 
